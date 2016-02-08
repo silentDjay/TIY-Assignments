8d70f578e325c3a824124daa4f0d028f31682bf1
Jeremy Smith <smithjeremym@gmail.com>
2016-02-08 18:29:40 -0500

    add comment to FiboNacho problem, add solution to hard mode

--- even-fibonacci.js
+++ even-fibonacci.js
@@ -1,3 +1,6 @@
+//By considering the terms in the Fibonacci sequence whose values
+// do not exceed 4 million, find the sum of the even-valued terms.
+
 var fibNacho = 1;
 var prevNacho = 0;
 var sumEvenNacho = 0;
@@ -6,7 +9,7 @@ while (fibNacho <= 4000000) {
     if (fibNacho % 2 === 0){
         sumEvenNacho = sumEvenNacho + fibNacho;
         console.log(" prevNacho = " + prevNacho + " fibNacho = " + fibNacho + " sumEvenNacho = " + sumEvenNacho);
-        //the previous line was simply to keep track of my variables as they iterated. It was VERY helpful.  
+        //the previous line was simply to keep track of my variables as they iterated. It was VERY helpful.
     }
     fibNachoKeep = fibNacho;
     fibNacho = fibNacho + prevNacho;
