bb0d5ff04e9abbf57ee162349900a683a13f1501
Jeremy Smith <smithjeremym@gmail.com>
2016-02-09 13:26:40 -0500

    update verbing exercise

--- verbingNoun.js
+++ verbingNoun.js
@@ -11,3 +11,22 @@ function verbing(noun){
 }
 
 console.log(verbing("yellow"));
+
+/**
+the live code on this page returns 'undefined'
+the code in the comment here runs, but it's buggy.
+
+function verbing(noun){
+    if (noun.length >= 3){
+        return noun + "ing";
+    }
+    if (noun.endsWith("ing")){
+        return noun + "ly";
+    }
+    else{
+        return noun;
+    }
+}
+
+console.log(verbing("yellow"));
+**/
