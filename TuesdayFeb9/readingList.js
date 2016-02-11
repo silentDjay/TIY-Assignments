6c9ae71505e013564f4341b0f192f5762b85860e
Jeremy Smith <smithjeremym@gmail.com>
2016-02-11 10:03:24 -0500

    make if criteria more concise

--- TuesdayFeb9/readingList.js
+++ TuesdayFeb9/readingList.js
@@ -10,15 +10,15 @@
 //  log a string like 'You still need to read "The Lord of the
 //  Rings" by J.R.R. Tolkien.'
 
-var bookList = [{a: "The Stinky Cheese Man and Other Fairly Stupid Tales", b: "by Jon Scieszka", c: "true"},
-{a: "Master and Margarita", b:"by Mikhail Bulgakov", c:"false"},
-{a: "Dave Barry in Cyberspace", b: "by Dave Barry", c: "true"},
-{a: "Cryptonomicon", b:"by Neal Stephenson", c: "false"},
-{a: "The Life and Opinions of Tristram Shandy, Gentleman", b:"by Laurence Sterne", c:"false"}
+var bookList = [{a: "The Stinky Cheese Man and Other Fairly Stupid Tales", b: "by Jon Scieszka", c: true},
+{a: "Master and Margarita", b:"by Mikhail Bulgakov", c: false},
+{a: "Dave Barry in Cyberspace", b: "by Dave Barry", c: true},
+{a: "Cryptonomicon", b:"by Neal Stephenson", c: false},
+{a: "The Life and Opinions of Tristram Shandy, Gentleman", b:"by Laurence Sterne", c:false}
 ]
 
 for (bookCounter = 0; bookCounter < bookList.length; bookCounter ++) {
-  if (bookList[bookCounter].c === "true" ){
+  if (bookList[bookCounter].c){
     console.log("You read " + bookList[bookCounter].a + " " + bookList[bookCounter].b + " a while back. It was okay.");
   }
   else {
