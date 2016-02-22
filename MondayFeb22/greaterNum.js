7bfad7ced9056d5c092126dc370c22f5a9806b8a
Jeremy Smith <smithjeremym@gmail.com>
2016-02-22 10:54:00 -0500

    improve classroom challenge solution

--- MondayFeb22/greaterNum.js
+++ MondayFeb22/greaterNum.js
@@ -1,5 +1,7 @@
 function greaterNum(num1, num2) {
-    if (num1 > num2) {
+    if (num1.isString || num2.isString){
+      return "Please provide numbers!"
+    } else if (num1 > num2) {
     return "The greater number of " + num1 + " and " + num2 + " is " + num1 + ".";
     } else if (num2 > num1) {
         return "The greater number of " + num1 + " and " + num2 + " is " + num2 + ".";
@@ -12,3 +14,4 @@ console.log(greaterNum(45, 89));
 console.log(greaterNum(-13, 11));
 console.log(greaterNum(.000000002, .000000001));
 console.log(greaterNum(9, 9));
+console.log(greaterNum("ponies", "yellow"));
