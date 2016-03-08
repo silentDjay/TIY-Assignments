fabeeea28ff06c6b13f43f889d03a0d1b6fb529e
Jeremy Smith <smithjeremym@gmail.com>
2016-03-08 10:58:47 -0500

    adding today's classroom challenge (piglatin translator)

---
+++ TuesdayMar8/pigLatein.js
@@ -0,0 +1,23 @@
+var pigArray = [];
+
+function pigLatein(str){
+    var strArray = str.split(" ");
+    // console.log(strArray);
+    // console.log(strArray[0].charAt(0));
+    // console.log([strArray[0],'ay'].join(""));
+
+    for (var i = 0; i < strArray.length; i++){
+        if (strArray[i].charAt(0) === "a" || strArray[i].charAt(0) === "e" || strArray[i].charAt(0) === "i" || strArray[i].charAt(0) === "o" || strArray[i].charAt(0) === "u"){
+        var pigString = [strArray[i],'ay'].join("");
+        console.log(pigString);
+        pigArray.push(pigString);
+        console.log(pigArray);
+        } else  { //if word starts with one or more consonants, move them to the end of the word and add 'ay'
+
+        }
+    }
+
+
+}
+
+pigLatein("apple pear thing cool");
