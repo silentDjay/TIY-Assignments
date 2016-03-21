6493954b8b4db00a520ded8640d594cef035e5d5
Jeremy Smith <smithjeremym@gmail.com>
2016-03-21 12:52:52 -0400

    add correct solution to today's classroom challenge

--- MondayMar21/palindromic.js
+++ MondayMar21/palindromic.js
@@ -1,23 +1,27 @@
 // find the largest palindromic number made from the product of two 3-digit numbers
-// var x = 100;
 
-for (x = 999; x > 99; x--){
-  for (y = 999; y > 99; y--){
-    var palindromeProd = x*y;
-    palindromeProd.toString();
-    console.log(palindromeProd);
-    console.log(palindromeProd.reverse());
-    if (palindromeProd % 2 === 0){
-      console.log(x*y);
-      break;
-    }else{
+function palindrome(){
+  var largestPal = 0;
+  for (x = 999; x > 99; x--){
+    for (y = 999; y > 99; y--){
+      var palindromeProd = (x*y).toString();
+      var palArray = palindromeProd.split("");
+      // console.log(palindromeProd);
+      var revProd = palArray.reverse().join("");
+      if (palindromeProd === revProd){
+        //   console.log(x*y);
+        //   console.log("x is "+ x + " and y is "+ y);
+        if ((x*y) > largestPal){
+          largestPal = x*y;
+        } else {
+          //
+        }
+      }else{
       //
     }
   }
 }
+return largestPal;
+}
 
-// function(num){
-//     if (num ){//check if the number is
-//         //
-//     } else if ()
-// }
+palindrome();
